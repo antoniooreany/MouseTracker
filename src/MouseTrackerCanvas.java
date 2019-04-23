9c595d5add10d3f7f260128a9a4b7de59aafafc9
antoniooreany <antonio_oreany@mail.ru>
2019-04-23 15:19:55 +0200

    smooth drawing arranged

--- src/MouseTrackerCanvas.java
+++ src/MouseTrackerCanvas.java
@@ -1,10 +1,14 @@
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 class MouseTrackerCanvas extends Canvas {
 
     private final GraphicsContext gc;
+    private double previousX = -1;
+    private double previousY = -1;
 
     private GraphicsContext getGc() {
         return gc;
@@ -14,13 +18,24 @@ class MouseTrackerCanvas extends Canvas {
         super(canvas_width, canvas_height);
         gc = getGraphicsContext2D();
         gc.setFill(Color.TRANSPARENT);
-        this.setOnMouseDragged(event -> {
-            this.getGc().setFill(Color.BLACK);
-            this.getGc().fillOval(event.getX(), event.getY(), 10, 10);
+        setOnMouseDragged(event -> {
+            getGc().setFill(Color.BLACK);
+            if (previousX >= 0 || previousY >= 0) {
+                getGc().strokeLine(previousX, previousY, event.getX(), event.getY());
+            }
+            previousX = event.getX();
+            previousY = event.getY();
+        });
+
+        setOnMouseMoved(event -> {
+            previousX = event.getX();
+            previousY = event.getY();
         });
     }
 
     void clear() {
-        gc.clearRect(0,0, this.getWidth(), this.getHeight());
+        gc.clearRect(0, 0, getWidth(), getHeight());
+        previousX = -1;
+        previousY = -1;
     }
 }
